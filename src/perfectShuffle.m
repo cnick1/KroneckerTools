a423a496604f31436ce9426131460f517521e25e
Nick Corbin <cnick1@vt.edu>
2023-04-05 20:25:46 -0700

    Replaced zeros with sparse from the beginning for ram usage

--- src/perfectShuffle.m
+++ src/perfectShuffle.m
@@ -29,12 +29,12 @@ function [S] = perfectShuffle(p,q)
   r = p*q;
   I = speye(r);
   
-  S = zeros(r,r);
+  S = sparse(r,r);
   
   for qq=1:q
     S( ((qq-1)*p+1):qq*p, : ) = I(qq:q:r,:);
   end
 
-  S = sparse(S);
+%   S = sparse(S);
 end
 
