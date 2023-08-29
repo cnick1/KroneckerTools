ffe2410a46c447383136f7201777e08cbbc10f5a
Nick Corbin <cnick1@vt.edu>
2023-08-29 15:00:32 -0700

    Got rid of transposing since coefficient is symmetric

--- src/kronPolyDerivEval.m
+++ src/kronPolyDerivEval.m
@@ -30,30 +30,13 @@ else
     end
 end
 
-%% Transpose the coefficients if required
-% Check dimensions of the last entry in c to see which dimension is the
-% appropriate size. To multiply, the second dimension  of c{k} has to be
-% length(z)^k; instead of checking all of them, just check the last
-% entry, with k=d=length(c) already having been defined.
-[nRows, nCols] = size(c{d});
-
-if (length(z) ^ d == nCols)
-    % No need to transpose
-elseif (length(z) ^ d == nRows)
-    % Need to transpose, otherwise dimensions will not work
-    c = cellfun(@transpose, c, 'UniformOutput', false);
-else
-    % Probably won't ever happen but good contingency
-    warning('Dimensions of polynomial coefficients are not consistent')
-end
-
 %% Perform polynomial evalauation
 %  Special case if the linear term is an empty cell
 n = size(z, 1);
 if isempty(c{1})
     x = 0;
 else
-    x = c{1};
+    x = c{1}.';
 end
 
 zkm1 = 1;
