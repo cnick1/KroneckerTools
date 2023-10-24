f7dd3d1637a8dc77686f5557c1141c271a055a34
Nick Corbin <cnick1@vt.edu>
2023-10-23 18:35:55 -0700

    Fixed bug in h2q indexing

--- util/h2q.m
+++ util/h2q.m
@@ -18,7 +18,7 @@ for k = 2:2*ell
     q{k} = sparse(n^k,1);
     
     % TODO: use symmetry to cut in half
-    for i = (k - ell):ell % would be 1:(k-1) but need to truncate only to h{} terms which exist
+    for i = max(1, k - ell):min(k-1, ell) % would be 1:(k-1) but need to truncate only to h{} terms which exist
         j = k - i;
         q{k} = q{k} + vec(h{i}.' * h{j});
     end
