0a88f5541f5cb66541f2fa811d2eb4df4c247cc5
Nick Corbin <cnick1@vt.edu>
2025-03-13 00:41:55 -0700

    Introduction of generalized Lyapunov solver based on QZ decomposition for incuding E matrix

--- src/KroneckerSumSolver.m
+++ src/KroneckerSumSolver.m
@@ -14,6 +14,9 @@ function [x] = KroneckerSumSolver(A,b,k,E,M,solver)
 %       E      - mass matrix, for generalized Bartels-Stewart
 %       M      - diagonal shift of the linear system.
 %       solver - string specifying the solver. Options:
+%                  • 'generalized-bartels-stewart' - Nicks's generalized
+%                     N-way Bartels-Stewart that includes E matrix. If an E
+%                     matrix is included, this solver will be selected.
 %                  • 'bartels-stewart' - Jeff's N-way Bartels-Stewart
 %                     direct solver from [1]. Described more below.
 %                  • 'chen-kressner' - calls laplace_merge() solver from
@@ -66,6 +69,7 @@ function [x] = KroneckerSumSolver(A,b,k,E,M,solver)
 if nargin < 6
     % solver = 'chen-kressner';
     solver = 'bartels-stewart';
+    % solver = 'generalized-bartels-stewart';
     if nargin < 4
         E = [];
         if nargin < 3
@@ -78,107 +82,273 @@ if length(b) == 1
     solver = 'bartels-stewart'; % chen-kressner solver is broken for n=1 case
 end
 
+if ~isempty(E)
+    solver = 'generalized-bartels-stewart';
+end
+
+n = size(A{1},1);
 switch solver
-    case 'bartels-stewart'
-            %% k-way Bartels-Stewart Algorithm
-            % We wish to solve the system ℒₖ(A)x=b. Let A=UTU'; then this is
-            % equivalent to
-            %       ℒₖ(A)x = (U⊗U⊗...⊗U)ℒₖ(T)(U⊗U⊗...⊗U)' x = b
-            % Now, we can multiply by the conjugate transpose
-            %       ℒ(T) (U⊗U⊗...⊗U)' x = (U⊗U⊗...⊗U)'b
-            %   ≡   ℒₖ(T)|------ y ------| = |------ b̅ -----|
-            % From here, we first solve this system for y via block back-
-            % substitution. Then, we can multiply to get x:
-            %       (U⊗U⊗...⊗U)' x = y   →   x = (U⊗U⊗...⊗U) y
+    case 'generalized-bartels-stewart'
+        %% Generalized k-way Bartels-Stewart Algorithm
+        % We wish to solve the system ℒₖᴱ(A)x=b, which is a generalized k-way Lyapunov equation
+        % (has a mass matrix E). Bartels-Stewart algorithm for solving consists of 3 steps:
+        %   1) Transform to the triangular form
+        %   2) Solve the transformed system
+        %   3) Transform the solution back
+        % These are done as follows:
+        %   1) Let A = Q'TₐZ' and E = Q'TₑZ' be the QZ decomposition of A,E; then the k-way
+        %      generalized Lyapunov equation is equivalent to
+        %         ℒₖᴱ(A)x = (Q⊗Q⊗...⊗Q)'ℒₖᵀᵉ(Tₐ)(Z⊗Z⊗...⊗Z)' x = b
+        %      We can multiply by the left Schur vectors Q to get the triangular form (sometimes
+        %      called reduced system)
+        %         ℒₖᵀᵉ(Tₐ) (Z⊗Z⊗...⊗Z)' x = (Q⊗Q⊗...⊗Q)b
+        %                  |------ y ------|  |------ b̅ -----|
+        %
+        %   2) Now we need to solve ℒₖᵀᵉ(Tₐ)y=b̅. Since ℒₖᵀᵉ(Tₐ) is block triangular, we can solve
+        %      this via block back substitution, where essentially as we solve for components of
+        %      y, we move them to the right-hand-side.
+        %
+        %   3) From y, we can multiply by the right Schur vectors Z to get x:
+        %       (Z⊗Z⊗...⊗Z)' x = y   →   x = (Z⊗Z⊗...⊗Z) y
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%    1) Transform to triangular form    %%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Get QZ decomposition of A,E
+        [Ta,Te,Q,Z] = qz(full(A{1}),full(E),'complex');
+        
+        % Replace linear system for x with linear system for y by multiplying by (Q⊗Q⊗...⊗Q)
+        b = kroneckerLeft(Q,b);
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%    2) Solve the transformed system    %%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Solve ℒₖᵀᵉ(Tₐ) y = b̅ via block back substitution. We will solve k-1 systems of dimension
+        % (n x n), starting with the last one and then going backwards (backward substitution). At
+        % each step, we need to form:
+        %   a) the coefficient matrix At for the current unknowns
+        %   2) the rhs vector, updated each time to include the previously computed quantities
+        
+        X = zeros(n,n^(k-1));
+        B = reshape(b,n,n^(k-1));
+        for colIdx = n^(k-1):-1:1
+            % ~~~~~~~~~~~~~~~~~~~~    a) Construct At    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            % At is an n x n diagonal block of ℒₖᵀᵉ(Tₐ); ℒₖᵀᵉ(Tₐ), where Tₐ and Tₑ are both upper
+            % triangular, is a  sum of a variety of permutations:
+            %     (Tₐ⊗Tₑ⊗...⊗Tₑ) + (Tₑ⊗Tₐ⊗...⊗Tₑ) + ... + (Tₑ⊗Tₑ⊗...⊗Tₐ)
+            % The ith diagonal blocks are therefore, by the definition of the Kronecker product,
+            %     (Tₐ⊗Tₑ⊗...)ᵢᵢ Tₑ + (Tₑ⊗Tₐ⊗...)ᵢᵢ Tₑ + ... + (Tₑ⊗Tₑ⊗...)ᵢᵢ Tₐ
+            % Notice that the only component that has "element-wise resolution" is the last one;
+            % the other components are all some scalar multiple of Tₑ. What that means is that At
+            % will always be α Tₐ + β Tₑ for some constant α and β; α and Tₐ and come from the
+            % last permutation, and the other k-1 permutations determine β and Tₑ.
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%% How to get the entry (A⁽¹⁾⊗A⁽²⁾⊗...⊗A⁽ᵏ⁾)ᵢⱼ without forming the whole thing? %%%
+            %%%     (A⁽¹⁾⊗A⁽²⁾⊗...⊗A⁽ᵏ⁾)ᵢⱼ = A⁽¹⁾ᵢ₁,ⱼ₁ * A⁽²⁾ᵢ₂,ⱼ₂ * ... * A⁽ᵏ⁾ᵢₖ,ⱼₖ          %%%
+            %%% for some row indices i₁,i₂,...,iₖ and column indices j₁,j₂,...,jₖ. Getting the  %%%
+            %%% indices turns out to be related to converting numbers to k-digit base n, and   %%%
+            %%% can be done as follows:                                                        %%%
+            %%%     row_indices = flip(tt_ind2sub(n*ones(1,k),i));                             %%%
+            %%%     col_indices = flip(tt_ind2sub(n*ones(1,k),j));                             %%%
+            %%% Evaluating product A⁽¹⁾ᵢ₁,ⱼ₁ * ... * A⁽ᵏ⁾ᵢₖ,ⱼₖ can be done with a simple loop:  %%%
+            %%%     ABij = 1;                                                                  %%%
+            %%%     for idx = 1:k                                                              %%%
+            %%%         ABij = ABij * matrices{idx}(row_indices(idx), col_indices(idx));       %%%
+            %%%     end                                                                        %%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
-            n = size(A{1},1);
+            % At is the colIdxth coefficient along the diagonal; both α and β use the same indices, just evaluating the product of different matrices
+            row_indices = flip(tt_ind2sub(n*ones(1,k-1),colIdx));
+            col_indices = flip(tt_ind2sub(n*ones(1,k-1),colIdx));
             
-            % Get Schur decomposition of A
-            [U,T] = schur(A{1},'complex');
-            
-            % Replace linear system for x with linear system for y by
-            % multiplying by (U⊗U⊗...⊗U)'
-            b = kroneckerLeft(U',b);
-            % [A{1:k}] = deal(T);
-            
-            if nargin < 5; UMU = sparse(n,n);
-            else; UMU = U'*M*U; end % diagonal shift; not currently used
-            
-            %% Solve ℒₖ(T) y = b̅ via block back substitution
-            % We will solve k-1 systems of dimension (n x n), starting
-            % with the last one and then going backwards (backward
-            % substitution). At each step, we need to form
-            %   1) the coefficient matrix At
-            %   2) the rhs vector, which is updated each time to include the
-            %      previously computed quantities
-            
-            X = zeros(n,n^(k-1));
-            B = reshape(b,n,n^(k-1));
-            jRange = cell(1,k);
-            for jj = n^(k-1):-1:1
-                % jIndex is jj in k-digit base n; ex. [2 2 2] = 8, [2 2 1]  = 7, etc.
-                jIndex = flip(tt_ind2sub(n*ones(1,k-1),jj));  
-                colIdx = tt_sub2ind(n*ones(1,k-1),jIndex);
-                
-                %% 1) Construct At, the upper triangular coefficient matrix for the current unknowns
-                % We know the coefficient is going to contain a
-                % contribution from T, since (I⊗I⊗...⊗T) is a block
-                % diagonal copying of T. However, we also need to
-                % accumulate the contributions from (T⊗I⊗...⊗I),
-                % (I⊗T⊗...⊗I), etc.
-                diagA = 0;
-                for i=1:k-1
-                    diagA = diagA + T(jIndex(i),jIndex(i));
-                end
-                At = T + diagA*eye(n) + UMU;
-                
-                %% 2) Construct rhs, the b̅ updated with the already computed unknowns
-                
-                rhs = B(:,colIdx);
-                
-                %  Back-substitution steps
-                for i=1:k-1    % this could be done by decreaseByOne as well
-                    jRange{i} = (jIndex(i)+1):n;
-                end
-                
-                for i=1:k-1
-                    if (~isempty(jRange{i}))
-                        shift = 1;
-                        for j=1:(i-1)
-                            shift = shift + (jIndex(j)-1)*n^(j-1);
-                        end
-                        for j=i:k-1
-                            shift = shift + (jIndex(j)-1)*n^(j-1);
-                        end
-                        jIdx = shift + (jRange{i}-1)*n^(i-1);
-                        
-                        rhs = rhs - X(:,jIdx)*T(jIndex(i),jRange{i}).';
-                    end
-                end
-                
-                X(:,colIdx) = At\rhs;
-                
+            % ---------------------------------    Getting α    ----------------------------------
+            % Here we just need to get (Tₑ⊗Tₑ⊗...⊗Tₑ)ᵢⱼ Tₐ
+            matrices = cell(1,k-1); % this is just for coding purposes, not much extra memory is allocated by this
+            [matrices{1:k-1}] = deal(Te);
+            alpha = 1;
+            for idx = 1:k-1
+                alpha = alpha * matrices{idx}(row_indices(idx), col_indices(idx));
             end
             
-            % Now solve for x given y by multiplying by (U⊗U⊗...⊗U)
-            x = real(kroneckerLeft(U,X(:)));
+            % ---------------------------------    Getting β    ----------------------------------
+            % Here we need to consider the k-1 other permutations
+            % (Tₐ⊗Tₑ⊗...⊗Tₑ)ᵢⱼ Tₑ, (Tₑ⊗Tₐ⊗...⊗Tₑ)ᵢⱼ Tₑ, ..., (Tₑ⊗Tₑ⊗...⊗Tₐ)ᵢⱼ Tₑ
+            beta = 1;
+            for idx2 = 1:k-1
+                matrices{idx2} = Ta; % set the idx2th matrix to Ta
+                for idx = 1:k-1
+                    beta = beta * matrices{idx}(row_indices(idx), col_indices(idx));
+                end
+                matrices{idx2} = Te; % reset the idx2th matrix to Te
+            end
             
+            At = alpha*Ta + beta*Te;
+            
+            % ~~~~~~~~~~~~~~~~~~~~~    b) Construct rhs vector    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            % This is the b̅ updated with the already computed unknowns
+            % Start with original b̅
+            rhs = B(:,colIdx);
+            
+            % Now update RHS by removing already computed things
+            for idx3 = n^(k-1):-1:colIdx+1
+                % The things we need to remove are n x n super-diagonal blocks of ℒₖᵀᵉ(Tₐ)
+                % multiplied with n x 1 blocks of X that have already been computed. Getting these
+                % blocks is similar to how we computed At, but now for off diagonal components.
+                % Corresponding to a particular component of X will be a matrix Aod, which again
+                % will have the form γ Tₐ + δ Tₑ for the same reasons as before. We just need to
+                % compute γ and δ for each of the already computed X and corresponding Aod
+                
+                row_indices = flip(tt_ind2sub(n*ones(1,k-1),colIdx));
+                col_indices = flip(tt_ind2sub(n*ones(1,k-1),idx3));
+                
+                % -------------------------------    Getting γ    --------------------------------
+                % Here we just need to get (Tₑ⊗Tₑ⊗...⊗Tₑ)ᵢⱼ Tₐ
+                matrices = cell(1,k-1); % this is just for coding purposes, not much extra memory is allocated by this
+                [matrices{1:k-1}] = deal(Te);
+                gamma = 1;
+                for idx = 1:k-1
+                    gamma = gamma * matrices{idx}(row_indices(idx), col_indices(idx));
+                end
+                
+                % -------------------------------    Getting δ    --------------------------------
+                % Here we need to consider the k-1 other permutations
+                delta = 1;
+                for idx2 = 1:k-1
+                    matrices{idx2} = Ta; % set the idx2th matrix to Ta
+                    for idx = 1:k-1
+                        delta = delta * matrices{idx}(row_indices(idx), col_indices(idx));
+                    end
+                    matrices{idx2} = Te; % reset the idx2th matrix to Te
+                end
+                
+                Aod = gamma*Ta + delta*Te;
+                rhs = rhs - Aod*X();
+            end
+            
+            % Solve for component of X
+            X(:,colIdx) = At\rhs; % At is upper triangular
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%    3) Transform solution back    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Now solve for x given y by multiplying by (Z⊗Z⊗...⊗Z)
+        x = real(kroneckerLeft(Z,X(:)));
+        
+    case 'bartels-stewart'
+        %% k-way Bartels-Stewart Algorithm
+        % We wish to solve the system ℒₖ(A)x=b. Bartels-Stewart
+        % algorithm for solving consists of 3 steps:
+        %   1) Transform to the triangular form
+        %   2) Solve the transformed system
+        %   3) Transform the solution back
+        % These are done as follows:
+        %   1) Let A=UTU' be the Schur decomposition of A; then the k-way
+        % Lyapunov equation is equivalent to
+        %       ℒₖ(A)x = (U⊗U⊗...⊗U)ℒₖ(T)(U⊗U⊗...⊗U)' x = b
+        % We can multiply by the conjugate transpose of the Schur vectors
+        % to get the triangular form (sometimes called reduced system)
+        %       ℒ(T) (U⊗U⊗...⊗U)' x = (U⊗U⊗...⊗U)'b
+        %   ≡   ℒₖ(T)|------ y ------| = |------ b̅ -----|
+        %
+        %   2) Now we need to solve ℒₖ(T)y=b̅. Since ℒₖ(T) is block
+        %   triangular, we can solve this via block back substitution,
+        %   where essentially as we solve for components of y, we move
+        %   them to the right-hand-side.
+        %
+        %   3) From y, we can multiply by the Schur vectors to get x:
+        %       (U⊗U⊗...⊗U)' x = y   →   x = (U⊗U⊗...⊗U) y
+        
+        %%% 1) Transform to triangular form %%%
+        % Get Schur decomposition of A
+        [U,T] = schur(A{1},'complex');
+        
+        % Replace linear system for x with linear system for y by
+        % multiplying by (U⊗U⊗...⊗U)'
+        b = kroneckerLeft(U',b);
+        
+        if nargin < 5; UMU = sparse(n,n);
+        else; UMU = U'*M*U; end % diagonal shift; not currently used
+        
+        %%% 2) Solve the transformed system %%%
+        % Solve ℒₖ(T) y = b̅ via block back substitution
+        % We will solve k-1 systems of dimension (n x n), moving things
+        % we compute to the right-hand-side (back substitution). At
+        % each step, we need to form
+        %   a) the coefficient matrix At for the current unknowns
+        %   b) the rhs vector, which is updated each time to include the
+        %      previously computed quantities
+        
+        X = zeros(n,n^(k-1));
+        B = reshape(b,n,n^(k-1));
+        jRange = cell(1,k-1);
+        for jj = n^(k-1):-1:1
+            % jIndex is jj in k-1-digit base n; ex. [2 2 2] = 8, [2 2 1]  = 7, etc.
+            jIndex = flip(tt_ind2sub(n*ones(1,k-1),jj));
+            colIdx = tt_sub2ind(n*ones(1,k-1),jIndex); % colIdx doesn't exactly follow jj; I think this is because there are zero blocks that can be skipped when E = I
+            
+            %%% a) Construct At %%%
+            % We know the coefficient is going to contain a
+            % contribution from T, since (I⊗I⊗...⊗T) is a block
+            % diagonal copying of T. However, we also need to
+            % accumulate the contributions from (T⊗I⊗...⊗I),
+            % (I⊗T⊗...⊗I), etc., which all appear along the diagonal
+            % since the last matrix is always I for these permutations
+            diagA = 0;
+            for i=1:k-1
+                diagA = diagA + T(jIndex(i),jIndex(i));
+            end
+            
+            At = T + diagA*eye(n) + UMU;
+            
+            %%% b) Construct rhs vector %%%
+            % This is the b̅ updated with the already computed unknowns
+            % Start with original b̅
+            rhs = B(:,colIdx);
+            
+            % Now update RHS by removing already computed things
+            for i=1:k-1
+                jRange{i} = (jIndex(i)+1):n;
+            end
+            
+            for i=1:k-1
+                if (~isempty(jRange{i}))
+                    shift = 1;
+                    for j=1:(i-1)
+                        shift = shift + (jIndex(j)-1)*n^(j-1);
+                    end
+                    for j=i:k-1
+                        shift = shift + (jIndex(j)-1)*n^(j-1);
+                    end
+                    jIdx = shift + (jRange{i}-1)*n^(i-1);
+                    
+                    rhs = rhs - X(:,jIdx)*T(jIndex(i),jRange{i}).';
+                end
+            end
+            
+            % Solve for component of X
+            X(:,colIdx) = At\rhs;
+            
+        end
+        
+        %%% 3) Transform solution back %%%
+        % Now solve for x given y by multiplying by (U⊗U⊗...⊗U)
+        x = real(kroneckerLeft(U,X(:)));
+        
     case 'chen-kressner'
+        %% Chen & Kressner recursive blocked algorithm
+        % The back substitution steps in Bartels-Stewart uses primarily level 2
+        % BLAS operations (matrix-vector multiplications). This paper
+        % proposes an algorithm that better leverages level 3 BLAS
+        % operations (is more memory efficient)
         try
             addpath('../tensor_recursive')
         catch
             disp("Download tensor_recursive package from https://www.epfl.ch/labs/anchp/index-html/software/misc/")
-            error("tensor_recursive not found, please download it.")
+            error("tensor_recursive not found, please add it to path.")
         end
         
-        n = length(A{1});
         B = reshape(b,n*ones(1,k));
         % if n^d < 2
         %     X = laplace_small(A, B);
         % else
         X = laplace_merge(A, B);
         % end
-        x = real(X(:)); % real seems to be needed, not sure why
+        x = real(X(:)); % real needed due to use of complex Schur decomposition
 end
 
 end
