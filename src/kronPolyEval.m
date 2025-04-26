7f3b320fc3506185a57af03e3c8bd94463334b80
Nick Corbin <cnick1@vt.edu>
2025-04-26 01:10:12 -0700

    Cleaned up kronPolyEval, preparing to add sparse evaluation

--- src/kronPolyEval.m
+++ src/kronPolyEval.m
@@ -1,124 +1,123 @@
-function [x] = kronPolyEval(c,z,degree)
-%kronPolyEval evaluates a Kronecker polynomial out to degree=degree
+function [FofX] = kronPolyEval(f,x,d)
+%kronPolyEval Evaluate a Kronecker polynomial f(x) to degree d.
 %
-%  Usage:
-%     x = kronPolyEval(c,z,degree);
+%   Usage:     FofX = kronPolyEval(f,x,d);
 %
-%  Input Variables:
-%     c - coefficients of the polynomial (cell array)
-%     z - value to calculate the polynomial at (vector)
-%     degree - polynomial will be evaluated out to degree=degree
-%              (default is the length of c)
+%   Input Variables:
+%     f - cell array containing function coefficients
+%     x - point at which to evaluate the Jacobian (vector)
+%     d - polynomial will be evaluated out to degree=d
+%         (default is the length of f)
 %
-%  Returns:  x = c{1}*z + c{2}*kron(z,z) + ... c{degree}*kron(...)
+%   Description: The function f(x) of interest is a polynomial
 %
-%  or optionally,
+%              f(x) = F₁x + F₂(x⊗x) + ... + Fd(x...⊗x)
 %
-%            x = c{1}.'*z + c{2}.'*kron(z,z) + ...
+%   This is evaluated efficiently by recycling (x⊗x) and just adding one
+%   more factor of x at a time. Kronecker polynomials are used throughout
+%   our work: we represent the dynamics, cost function, value function, and
+%   optimal control as Kronecker polynomials. We can also represent coordinate
+%   transformations, energy functions, etc. This function kronPolyEval() can
+%   be used to evaluate all of these, which leads to some special cases:
+%       - Sometimes we may wish to evaluate only out to a certain degree,
+%         e.g. if you have a polynomial feedback law but just want the
+%         first term. The third input argument d permits this.
+%       - Some function, like the value function, do not have a linear
+%         component. If f{1} = [], it will be skipped.
+%       - If reduction is used, we may wish to evaluate the value function
+%         or gain using the reduced-order x≈Txᵣ just for the higher terms:
+%            V(x) =    1/2 ( v₂ᵀ(x⊗x) + v₃ᵣᵀ(xᵣ⊗xᵣ⊗xᵣ) + ... +   vᵣᵈᵀ(...⊗xᵣ) )
+%            u(x) = K₁ x + K₂ᵣ(xᵣ⊗xᵣ) +  K₃ᵣ(xᵣ⊗xᵣ⊗xᵣ) + ... +  Kᵣᵈ⁻¹(...⊗xᵣ) )
+%         This function can handle these cases using the factoredGainArray class
+%         and factoredValueArray class, which basically pass along the reduced
+%         coefficients and the reduction basis T.
 %
-%  We optionally handle the special case c{1} = [] by starting with the
-%  quadratic term for applications such as energy functions or value functions.
+%   Author: Rewritten by Nick Corbin, UCSD
+%           Based on original version by Jeff Borggaard, Virginia Tech
 %
-%  Author: Jeff Borggaard, Virginia Tech
-%          modified by Nick Corbin, UCSD
+%   License: MIT
 %
-%  Licence: MIT
-%
-%  Part of the KroneckerTools repository.
+%   Part of the KroneckerTools repository.
 %%
-
-d = length(c);
+lf = length(f);
 if (nargin<3)
-    degree = d;
-else
-    if (d<degree)
-        error('kronPolyEval: not enough entries in the coefficient cell array')
-    end
+    d = lf;
+end
+if (lf<d)
+    error('kronPolyEval: not enough entries in the coefficient cell array')
+end
+if d == 0
+    FofX = 0;
+    return
 end
 
-if degree == 0
-    x = 0; 
-    return 
-end
-
-if isa(c,'factoredGainArray')
-    %% Transpose the coefficients if required
-    % ommitted for now, assuming it is correct
-
-    %% Perform polynomial evalauation
-
-    %  Special case if the linear term is an empty cell
-    if isempty(c{1})
-        n = size(c{2},1);
-        x = zeros(n,1);
-    else
-        x = c{1}*z;
+if isa(f,'factoredGainArray')
+    %% Evaluate polynomial feedback law
+    %  u(x) = K₁ x + K₂ᵣ(xᵣ⊗xᵣ) +  K₃ᵣ(xᵣ⊗xᵣ⊗xᵣ) + ... +  Kᵣᵈ⁻¹(...⊗xᵣ) )
+    % using factoredGainArray class and reduced-order xᵣ
+    
+    % Evaluate linear term (can't be empty for u(x))
+    FofX = f{1}*x;
+    
+    % Get reduced-order state xᵣ and evaluate higher-degree terms
+    x = f.Tinv*x; xk = x;
+    for k=2:d
+        xk = kron(xk,x);
+        FofX = FofX + f.ReducedGains{k}*xk;
     end
-
-    z = c.Tinv*z;     zk = z;
-    for k=2:degree
-        zk = kron(zk,z);
-        x = x + c.ReducedGains{k}*zk;
+elseif isa(f,'factoredValueArray')
+    %% Evaluate polynomial value function
+    %  V(x) = 1/2 ( v₂ᵀ(x⊗x) + v₃ᵣᵀ(xᵣ⊗xᵣ⊗xᵣ) + ... + vᵣᵈᵀ(...⊗xᵣ) )
+    % using factoredValueArray class and reduced-order xᵣ
+    
+    % Transpose the coefficients v₂ᵀ,...,vᵣᵈᵀ
+    f.ReducedValueCoefficients = cellfun(@transpose,f.ReducedValueCoefficients,'UniformOutput',false);
+    
+    % First term in V(x) is quadratic term
+    FofX = f.ReducedValueCoefficients{2}*kron(x,x);
+    
+    % Get reduced-order state xᵣ and evaluate higher-degree terms
+    x = f.Tinv*x; xk = kron(x,x);
+    for k=3:d
+        xk = kron(xk,x);
+        FofX = FofX + f.ReducedValueCoefficients{k}*xk;
     end
-
-elseif isa(c,'factoredValueArray')
-    %% Transpose the coefficients
-    c.ReducedValueCoefficients = cellfun(@transpose,c.ReducedValueCoefficients,'UniformOutput',false);
-
-    %% Perform polynomial evalauation
-
-    %  Special case if the linear term is an empty cell
-    if isempty(c{1})
-        n = size(c{2},1);
-        x = zeros(n,1);
-    else
-        x = c{1}*z;
-    end
-
-    % Quadratic term
-    x  = x + c.ReducedValueCoefficients{2}*kron(z,z);
-      
-    z = c.Tinv*z; zk = kron(z,z);
-    for k=3:degree
-        zk = kron(zk,z);
-        x = x + c.ReducedValueCoefficients{k}*zk;
-    end
-
 else
+    %% Evaluate standard Kronecker polynomial
     %% Transpose the coefficients if required
-    % Check dimensions of the last entry in c to see which dimension is the
+    % Check dimensions of the last entry in f to see which dimension is the
     % appropriate size. To multiply, the second dimension  of c{k} has to be
-    % length(z)^k; instead of checking all of them, just check the last
-    % entry, with k=d=length(c) already having been defined.
-    [nRows, nCols] = size(c{d});
-
-    if (length(z)^d == nCols)
+    % length(x)^k; instead of checking all of them, just check the last
+    % entry, with k=lf=length(f) already having been defined.
+    [nRows, nCols] = size(f{lf});
+    
+    if (length(x)^lf == nCols)
         % No need to transpose
-    elseif (length(z)^d == nRows)
+    elseif (length(x)^lf == nRows)
         % Need to transpose, otherwise dimensions will not work
-        c = cellfun(@transpose,c,'UniformOutput',false);
+        f = cellfun(@transpose,f,'UniformOutput',false);
         %       warning('Coefficient needed to be transposed; consider transposing to speed up.')
     else
         % Probably won't ever happen but good contingency
         % warning('Dimensions of polynomial coefficients are not consistent')
     end
-
-    %% Perform polynomial evalauation
-
-    %  Special case if the linear term is an empty cell
-    if isempty(c{1})
-        n = size(c{2},1);
-        x = zeros(n,1);
+    
+    %% Perform polynomial evaluation
+    % Evaluate linear term; handle special case if the linear term is an empty cell
+    if isempty(f{1})
+        n = size(f{2},1);
+        FofX = zeros(n,1);
     else
-        x = c{1}*z;
+        FofX = f{1}*x;
     end
-
-    zk = z;
-    for k=2:degree
-        zk = kron(zk,z);
-        x  = x + c{k}*zk;
+    
+    % Evaluate higher-degree terms successively
+    xk = x;
+    for k=2:d
+        xk = kron(xk,x);
+        FofX  = FofX + f{k}*xk;
     end
-
+    
 end
 
 end
---
+++ util/jcbn.m
@@ -0,0 +1,67 @@
+function J = jcbn(F, x, symmetrize)
+%jcbn Return the Jacobian J(x) = ∂f(x)/∂x of the function f(x) evaluated at x.
+%
+%   Usage:  J = jcbn(F, x)
+%
+%   Inputs:    F - cell array containing function coefficients
+%              x - point at which to evaluate the Jacobian
+%     symmetrize - boolean, symmetrize rows of F if they aren't symmetric
+%
+%   Description: The Jacobian J(x) is the matrix giving the partial derivatives
+%   ∂f(x)/∂x of the function f(x). The function of interest is a polynomial
+%
+%              f(x) = F₁x + F₂(x⊗x) + ... + Fd(x...⊗x)
+%
+%   where the rows of the function coefficients are symmetrized. Then,
+%   the Jacobian is given by
+%
+%       J(x) = ∂f(x)/∂x = F₁ + 2F₂(I⊗x) + ... + d Fd(I...⊗x)
+%
+%   Evaluating this explicitly is expensive, so this function uses the
+%   Kronecker-vec identity to do this recursively and in a more efficient
+%   manner.
+%
+%   References: [1]
+%
+%   Part of the KroneckerTools repository.
+%%
+if nargin < 3
+    symmetrize = false;
+end
+
+n = size(x, 1);
+
+if symmetrize
+    if isempty(F{1})
+        J = 0;
+    else
+        J = 1/2*(F{1}+F{1}.');
+    end
+    
+    xkm1 = 1;
+    for k = 2:length(F)
+        % Using kron-vec identity
+        xkm1 = kron(xkm1, x);
+        % Need to iterate over n rows to apply kron-vec identity
+        for j = 1:n
+            J(j,:) = J(j,:) + k * xkm1.' * reshape(kronMonomialSymmetrize(F{k}(j,:),n,k),n^(k-1),[]); %#ok<AGROW>
+        end
+    end
+else
+    if isempty(F{1})
+        J = 0;
+    else
+        J = F{1};
+    end
+    
+    xkm1 = 1;
+    for k = 2:length(F)
+        % Using kron-vec identity
+        xkm1 = kron(xkm1, x);
+        % Need to iterate over n rows to apply kron-vec identity
+        for j = 1:n
+            J(j,:) = J(j,:) + k * xkm1.' * reshape(F{k}(j,:),n^(k-1),[]); %#ok<AGROW>
+        end
+    end
+end
+end
