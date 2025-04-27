161851d00748592a0327d83f9adb83c9c1cb2408
Nick Corbin <cnick1@vt.edu>
2025-04-26 17:13:17 -0700

    Added documentation to factoredGain/ValueArray

--- util/factoredGainArray.m
+++ util/factoredGainArray.m
@@ -1,24 +1,52 @@
 classdef factoredGainArray < matlab.mixin.indexing.RedefinesBrace
     %FACTOREDGAINARRAY Array of polynomial coefficients with reduction basis
     %factor.
-    %   Detailed explanation goes here
-
+    %   This class is designed to aid in computing, storing, and evaluating
+    %   efficiently the feedback law in PPR when reduction is used. The
+    %   feedback law is written in an approximate form in terms of the
+    %   reduced-order state x≈Txᵣ as
+    %       u(x) = K₁ x + K₂ᵣ(xᵣ⊗xᵣ) +  K₃ᵣ(xᵣ⊗xᵣ⊗xᵣ) + ... +  Kᵣᵈ⁻¹(...⊗xᵣ) )
+    %   The full-order gain coefficients are therefore approximated as
+    %       K₁ = K₁
+    %       K₂ ≈ K₂ᵣ(T⁻¹⊗T⁻¹)                (Note: in practice, the T we are
+    %       K₃ ≈ K₃ᵣ(T⁻¹⊗T⁻¹⊗T⁻¹)            using is orthogonal, so T⁻¹=Tᵀ)
+    %   This class stores the gains {K₁,K₂ᵣ,...} and the transformation
+    %   coefficient (and its inverse) {T,T⁻¹}.
+    %
+    %   An important feature of this class is its integration with the
+    %   kronPolyEval() function. To evaluate u(x), kronPolyEval()
+    %   identifies if K is a factoredGainArray, in which case it
+    %   efficiently evaluates u(x) by handling the reduced-order state
+    %   approximation for the higher-order coefficients. For example,
+    %   K₃ ≈ K₃ᵣ(T⁻¹⊗T⁻¹⊗T⁻¹) is never formed; we instead form xᵣ=T⁻¹x and
+    %   evaluate the higher-order terms directly in terms of xᵣ, which is
+    %   much more efficient. This class allows the user to not have to
+    %   worry about these details.
+    %
+    %   For convenience, this class also overloads the following methods so
+    %   that a factoredGainArray behaves similarly to a regular cell array:
+    %       - brace indexing: permits indexing as if the array were a
+    %               regular cell array (currently only for single indexing)
+    %       - disp: provides option for displaying the reduced OR the
+    %               approximation of the full coefficients
+    
     properties
         ReducedGains
         Tinv
     end
-
+    
     methods (Access=protected)
+        % Overloading {} indexing
         function varargout = braceReference(obj,indexOp)
-            % [varargout{1:nargout}] = obj.ReducedGains.(indexOp); % Reduced gain 
+            % [varargout{1:nargout}] = obj.ReducedGains.(indexOp); % Reduced gain
             k = indexOp.Indices{1}; % Hardcoded only for single indexing
-            if k == 1 
+            if k == 1
                 [varargout{1:nargout}] = obj.ReducedGains.(indexOp);
-            else 
+            else
                 [varargout{1:nargout}] = calTTv({obj.Tinv}, k, k, obj.ReducedGains{k}.').'; % Full gain
             end
         end
-
+        
         function obj = braceAssign(obj,indexOp,varargin)
             if isscalar(indexOp)
                 [obj.ReducedGains.(indexOp)] = varargin{:};
@@ -26,7 +54,7 @@ classdef factoredGainArray < matlab.mixin.indexing.RedefinesBrace
             end
             [obj.ReducedGains.(indexOp)] = varargin{:};
         end
-
+        
         function n = braceListLength(obj,indexOp,indexContext)
             n = listLength(obj.ReducedGains,indexOp,indexContext);
         end
@@ -34,16 +62,28 @@ classdef factoredGainArray < matlab.mixin.indexing.RedefinesBrace
     methods (Access=public)
         function obj = factoredGainArray(ReducedGains,Tinv)
             %FACTOREDGAINARRAY Construct an instance of this class
-            %   Detailed explanation goes here
+            %   A factoredGainArray is just a set of reduced gains and the
+            %   transformation that converts from the full state to the
+            %   reduced state. Currently, it is just a simple struct with
+            %   two components:
+            %       ReducedGains - cell array of the reduced gains
+            %       Tinv         - (r x n) matrix used to construct the
+            %                      reduced state xᵣ=T⁻¹x
             obj.ReducedGains = ReducedGains;
             obj.Tinv = Tinv;
         end
-
+        
         function disp(obj, dispFull)
+            %DISP Overloaded method to display a factoredGainArray
+            % Optional argument:
+            %   dispFull - boolean, defaults to false. If true, then
+            %              Kᵢ ≈ Kᵢᵣ(T⁻¹⊗...⊗T⁻¹) will all be formed
+            %              explicitly and displayed. Note: this is just for
+            %              debugging, it should never typically be done.
             if nargin < 2
                 dispFull = false;
             end
-
+            
             if dispFull
                 fullGains = cell(1,length(obj.ReducedGains));
                 fullGains{1} = obj.ReducedGains{1};
@@ -53,12 +93,12 @@ classdef factoredGainArray < matlab.mixin.indexing.RedefinesBrace
                 disp(fullGains)
             else
                 builtin('disp', obj);
-                fprintf("    Call disp(Gains,true) to display the full coefficient array.\n\n")
+                fprintf("    Call disp(K,true) to display the full coefficient array.\n\n")
             end
         end
-
+        
         function result = length(obj)
-            result = length(obj.ReducedGains);    
+            result = length(obj.ReducedGains);
         end
     end
 end
--- util/factoredValueArray.m
+++ util/factoredValueArray.m
@@ -2,24 +2,50 @@ classdef factoredValueArray < matlab.mixin.indexing.RedefinesParen & ...
         matlab.mixin.indexing.RedefinesBrace
     %FACTOREDVALUEARRAY Array of polynomial coefficients with reduction basis
     %factor.
-    %   Detailed explanation goes here
-
+    %   This class is designed to aid in computing, storing, and evaluating
+    %   efficiently the value function in PPR when reduction is used. The
+    %   value function is written in an approximate form in terms of the
+    %   reduced-order state x≈Txᵣ as
+    %       V(x) =    1/2 ( v₂ᵀ(x⊗x) + v₃ᵣᵀ(xᵣ⊗xᵣ⊗xᵣ) + ... +   vᵣᵈᵀ(...⊗xᵣ) )
+    %   The full-order value coefficients are therefore approximated as
+    %       v₂ = v₂                           (Note: in practice, the T we are
+    %       v₃ ≈ (T⁻¹⊗T⁻¹⊗T⁻¹)ᵀ v₃ᵣ           using is orthogonal, so T⁻¹=Tᵀ)
+    %   This class stores the gains {K₁,K₂ᵣ,...} and the transformation
+    %   coefficient (and its inverse) {T,T⁻¹}.
+    %
+    %   An important feature of this class is its integration with the
+    %   kronPolyEval() function. To evaluate V(x), kronPolyEval()
+    %   identifies if K is a factoredValueArray, in which case it
+    %   efficiently evaluates V(x) by handling the reduced-order state
+    %   approximation for the higher-order coefficients. For example,
+    %   v₃ ≈ v₃ᵣ(T⁻¹⊗T⁻¹⊗T⁻¹) is never formed; we instead form xᵣ=T⁻¹x and
+    %   evaluate the higher-order terms directly in terms of xᵣ, which is
+    %   much more efficient. This class allows the user to not have to
+    %   worry about these details.
+    %
+    %   For convenience, this class also overloads the following methods so
+    %   that a factoredValueArray behaves similarly to a regular cell array:
+    %       - brace indexing: permits indexing as if the array were a
+    %               regular cell array (currently only for single indexing)
+    %       - parenthesis indexing: permits indexing as if the array were a
+    %               regular cell array (currently only for single indexing)
+    %       - disp: provides option for displaying the reduced OR the
+    %               approximation of the full coefficients
     properties
         ReducedValueCoefficients
         Tinv
     end
-
+    
     methods (Access=protected)
         function varargout = braceReference(obj,indexOp)
-            % [varargout{1:nargout}] = obj.ReducedGains.(indexOp); % Reduced gain 
             k = indexOp.Indices{1}; % Hardcoded only for single indexing
             if k == 1 || k == 2
                 [varargout{1:nargout}] = obj.ReducedValueCoefficients.(indexOp);
-            else 
-                [varargout{1:nargout}] = calTTv({obj.Tinv}, k, k, obj.ReducedValueCoefficients{k}); % Full gain
+            else
+                [varargout{1:nargout}] = calTTv({obj.Tinv}, k, k, obj.ReducedValueCoefficients{k}); % Full value fun
             end
         end
-
+        
         function obj = braceAssign(obj,indexOp,varargin)
             if isscalar(indexOp)
                 [obj.ReducedValueCoefficients.(indexOp)] = varargin{:};
@@ -27,17 +53,17 @@ classdef factoredValueArray < matlab.mixin.indexing.RedefinesParen & ...
             end
             [obj.ReducedValueCoefficients.(indexOp)] = varargin{:};
         end
-
+        
         function n = braceListLength(obj,indexOp,indexContext)
             n = listLength(obj.ReducedValueCoefficients,indexOp,indexContext);
         end
-
-         % Overloading () indexing
+        
+        % Overloading () indexing
         function out = parenReference(obj, indexOp)
             out = obj;
             out.ReducedValueCoefficients = obj.ReducedValueCoefficients.(indexOp);
         end
-
+        
         function obj = parenAssign(obj, indexOp, varargin)
             indices = indexOp.Indices{1};
             if isnumeric(indices) && numel(indices) == numel(varargin)
@@ -48,24 +74,31 @@ classdef factoredValueArray < matlab.mixin.indexing.RedefinesParen & ...
                 error("Indexing must match the number of input values.")
             end
         end
-
+        
         function n = parenListLength(~,~,~)
             n = 1;
         end
-
+        
         function obj = parenDelete(obj,indexOp)
-            obj.ReducedValueCoefficients.(indexOp) = [];        
+            obj.ReducedValueCoefficients.(indexOp) = [];
         end
-
+        
     end
     methods (Access=public)
         function obj = factoredValueArray(ReducedValueCoefficients,Tinv)
-            %FACTOREDGAINARRAY Construct an instance of this class
-            %   Detailed explanation goes here
+            %FACTOREDVALUEARRAY Construct an instance of this class
+            %   A factoredValueArray is just a set of reduced value
+            %   function coefficients and the transformation that converts
+            %   from the full state to the reduced state. Currently, it is
+            %   just a simple struct with two components:
+            %       ReducedValueCoefficients - cell array of the reduced
+            %                                  value function coefficients
+            %       Tinv         - (r x n) matrix used to construct the
+            %                      reduced state xᵣ=T⁻¹x
             obj.ReducedValueCoefficients = ReducedValueCoefficients;
             obj.Tinv = Tinv;
         end
-
+        
         function out = cat(~,varargin)
             out = factoredValueArray;
             for ix = 1:length(varargin)
@@ -77,16 +110,22 @@ classdef factoredValueArray < matlab.mixin.indexing.RedefinesParen & ...
                 end
             end
         end
-
+        
         function varargout = size(obj,varargin)
             [varargout{1:nargout}] = size(obj.ReducedValueCoefficients,varargin{:});
         end
-
+        
         function disp(obj, dispFull)
+            %DISP Overloaded method to display a factoredValueArray
+            % Optional argument:
+            %   dispFull - boolean, defaults to false. If true, then
+            %              vᵢ ≈ (T⁻¹⊗...⊗T⁻¹)ᵀ vᵢᵣ will all be formed
+            %              explicitly and displayed. Note: this is just for
+            %              debugging, it should never typically be done.
             if nargin < 2
                 dispFull = false;
             end
-
+            
             if dispFull
                 fullCoeffs = cell(1,length(obj.ReducedValueCoefficients));
                 fullCoeffs{1} = obj.ReducedValueCoefficients{1};
@@ -99,9 +138,9 @@ classdef factoredValueArray < matlab.mixin.indexing.RedefinesParen & ...
                 fprintf("    Call disp(v,true) to display the full coefficient array.\n\n")
             end
         end
-
+        
         function result = length(obj)
-            result = length(obj.ReducedValueCoefficients);    
+            result = length(obj.ReducedValueCoefficients);
         end
     end
 end
